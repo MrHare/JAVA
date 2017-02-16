17cf8c3254f512a9e98c844321a3d0cfc134f176
MrHare <584854989@qq.com>
2017-02-16 19:56:07 +0800

    leetcode14&70&83

--- LeetCode.java
+++ LeetCode.java
@@ -5,31 +5,23 @@ class ListNode{
 	ListNode(int x){val=x;}
 }
 public class LeetCode{
-	public ListNode reverseKGroup(ListNode head, int k) {
-		ListNode h=new ListNode(0);
-		boolean isEnd=false;
-		h.next=head;
-		ListNode p,q,tmp1,tmp2;/////////p the node 1,q the node k
-		p=h;
-		while(p.next!=null){
-			tmp1=p.next;
-			if(tmp1.next!=null){
-				tmp2=tmp1.next;
-			}else{
-				break;
-			}
-			tmp1.next=tmp2.next;
-			tmp2.next=tmp1;
-			p.next=tmp2;
-			p=tmp1;
-		}
-		return h.next;
+	 public ListNode deleteDuplicates(ListNode head) {
+        ListNode result=new ListNode(0);
+        result.next=head;
+        while(head.next!=null){
+        	if(head.val==head.next.val){
+        		head.next=head.next.next;
+        	}else{
+        		head=head.next;
+        	}
+        }
+        return result.next;
     }
 	public static void main(String[] args){
-		ListNode n1=new ListNode(1);
+		ListNode n1=new ListNode(2);
 		ListNode n2=new ListNode(2);
-		ListNode n3=new ListNode(3);
-		ListNode n4=new ListNode(4);
+		ListNode n3=new ListNode(2);
+		ListNode n4=new ListNode(2);
 		ListNode n5=new ListNode(5);
 		ListNode n6=new ListNode(6);
 		ListNode h;
@@ -39,7 +31,7 @@ public class LeetCode{
 		n4.next=n5;
 		n5.next=n6;
 		LeetCode leetCode=new LeetCode();
-		h=leetCode.reverseKGroup(n1,2);
+		h=leetCode.deleteDuplicates(n1);
 		while(h!=null){
 			System.out.println(h.val);
 			h=h.next;
