2356bf7dc8d2db6ead9933c5c00d70c35f9698a1
MrHare <584854989@qq.com>
2017-03-25 21:58:54 +0800

    lc142

---
+++ leetcode/142Linked List Cycle2.java
@@ -0,0 +1,33 @@
+/*
+METHOD ONE(22.59% 1ms T1)
+*/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                fast=head;
+                while(fast!=slow){
+                    fast=fast.next;
+                    slow=slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
