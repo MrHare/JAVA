deba1596bb9830c298116bf0ab94208155eee091
MrHare <584854989@qq.com>
2017-03-25 15:28:15 +0800

    lc190

---
+++ leetcode/190Reverse Bits.java
@@ -0,0 +1,15 @@
+/*
+METHOD ONE(48.46% 2ms T2)
+*/
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int result=0;
+        for(int i=0;i<32;i++){
+            result=result<< 1;
+            result=result|n&1;
+            n=n>> 1;
+        }
+        return result;
+    }
+}
