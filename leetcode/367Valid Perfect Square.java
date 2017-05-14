979336a2bd2e41267db9d3175d0e169beedc7f45
MrHare <584854989@qq.com>
2017-05-14 17:35:55 +0800

    lc367AM

--- leetcode/367Valid Perfect Square.java
+++ leetcode/367Valid Perfect Square.java
@@ -12,3 +12,34 @@ public class Solution {
         return false;
     }
 }
+//////////////////////
+/*
+little change based on METHOD ONE
+*/
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num==1)return true;// when num=1,ret=0,then num/0,so ..
+        int ret=num/2;
+        while(ret>num/ret){
+            ret=(ret+num/ret)/2;
+        };
+        return ret*ret==num;
+    }
+}
+///////////////////////
+/*
+METHOND TWo(12.53% 2ms)
+tips:
+1=1
+4=1+3
+8=1+3+5
+16=1+3+5+7..
+*/
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        for(int i=1;num>0;i+=2){
+            num-=i;
+        }
+        return num==0;
+    }
+}
