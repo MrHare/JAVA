e01b3265fb8034d6100377202ceea22f8ad8d169
MrHare <584854989@qq.com>
2017-05-21 19:54:46 +0800

    lc219

---
+++ leetcode/219Contains Duplicate2.java
@@ -0,0 +1,16 @@
+/*
+METHOD ONE(54.39% 19ms)
+*/
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])&&i-map.get(nums[i])<=k){
+                return true;
+            }else{
+                map.put(nums[i],i);
+            }
+        }
+        return false;
+    }
+}
